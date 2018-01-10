a13b87846ab64bffbd956f6f06268e47d201af47
codiechanel <codiechanel@gmail.com>
2018-01-10 08:53:52 +0800

    great

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const styles = {
 const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
-    <Hello name="coolness nice the best amazing" />
+    <Hello name="coolness nice the sure best amazing hmmm" />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
   </div>
 );
